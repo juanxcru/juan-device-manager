efc77fc74848c0cda2d812537d6cb19f4a1ce165
juanxcru <dinarjuanc@gmail.com>
2025-12-26 19:47:40 -0300

    primer approach de la solucion

---
+++ src/main/java/com/juan/devicemanage/domain/model/PosDetail.java
@@ -0,0 +1,41 @@
+package com.juan.devicemanage.domain.model;
+
+import com.juan.devicemanage.domain.model.enums.CardReadCap;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class PosDetail {
+
+    @Id
+    @Column(name = "device_pk", nullable = false)
+    private UUID id;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "device_pk",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_pos_details_device")
+    )
+    private Device device;
+
+
+    @NotBlank
+    @Column(name = "embedded_software_vendor", nullable = false, length = 80)
+    private String embeddedSoftwareVendor;
+
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "pos_card_capability",
+            joinColumns = @JoinColumn(name = "pos_device_pk", foreignKey = @ForeignKey(name = "fk_pos_read_capability_pos"))
+    )
+    @Column(name = "read_capability", nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private Set<CardReadCap> cardReadCapabilities = new HashSet<>();
+
+}
---
+++ src/main/java/com/juan/devicemanage/domain/model/enums/DeviceType.java
@@ -0,0 +1,5 @@
+package com.juan.devicemanage.domain.model.enums;
+
+public enum DeviceType {
+    ATM, POS
+}
